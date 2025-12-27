0d33bb619fd2340587c8edd9c30bc60f5480db0d
dawnofthed network <130524584+dawnofthed@users.noreply.github.com>
2025-12-27 02:47:22 -0500

    Delete astro-i18next.config.ts

--- astro-i18next.config.ts
+++
@@ -1,6 +0,0 @@
-/** @type {import('astro-i18next').AstroI18nextConfig} */
-export default {
-    defaultLocale: "en",
-
-    locales: ["en", "en-GB", "es", "es-LAT", "fr", "ja", "pt-PT", "pt-BR", "ca", "va", "frp", "de", "it", "fi", "ru", "vi", "tr", "hu", "ko-KR"],
-  };
